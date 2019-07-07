bde35fcd3870845dc1dd37b920c833221e3e6553
Benjamin Schwetz <benjamin.schwetz@gmx.de>
2019-07-07 12:00:30 +0200

    init

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onGesture(Gesture.Shake, function () {
+    Roll = Math.randomRange(0, 5)
+    myImage.showImage(Roll * 5)
+})
+let Roll = 0
+let myImage = images.createBigImage(`
+..... ..... ..... ..... ..... .....
+..... ...#. ...#. .#.#. .#.#. .#.#.
+..#.. ..... ..#.. ..... ..#.. .#.#.
+..... .#... .#... .#.#. .#.#. .#.#.
+..... ..... ..... ..... ..... .....
+`)
